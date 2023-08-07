9ddc439f2408e17dad2eb95a4dc4ee4d9e798e74
Vinicius Finger <fingervinicius@icloud.com>
2023-08-07 11:24:34 -0300

    verifying if a word is palindrome

---
+++ HackerRank/JavaStringReversePalindrome.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class JavaStringReversePalindrome {
+//Given a string , print Yes if it is a palindrome, print No otherwise.
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        scanner.close();
+        
+        StringBuilder stringBuilder = new StringBuilder(word);
+        String reverseWord = stringBuilder.reverse().toString();
+
+        if (word.equals(reverseWord)){
+            System.out.println("Yes");
+        } else { 
+            System.out.println("No");
+        }
+    }
+}
